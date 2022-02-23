0ff16a731b203cb22b089c50d684ba2ffc7de269
M_525526 <m_525526@ambgb000477.home>
2022-02-23 09:57:22 +0000

    ISDB-3: Intializing web app

--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -8,7 +8,7 @@ export class AppInsights {
       appInsights.setup(config.get('appInsights.instrumentationKey')).setSendLiveMetrics(true).start();
 
       appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] =
-        'rpe-expressjs-template';
+        'ds-ui';
       appInsights.defaultClient.trackTrace({
         message: 'App insights activated',
       });
