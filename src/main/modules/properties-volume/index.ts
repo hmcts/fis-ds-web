4af3e20bf26abee9a0026b6faacc14a9ed2bfffb
M_183792 <purushotham.kodatala@hmcts.net>
2022-03-04 17:11:12 +0000

    updated idam id and serect

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -19,13 +19,13 @@ export class PropertiesVolume {
       this.setSecret('secrets.adoption.postcode-lookup-token', 'services.postcodeLookup.token');
       this.setSecret('secrets.adoption.adoption-pcq-token', 'services.equalityAndDiversity.tokenKey');
     } else {
-      this.setLocalSecret('idam-secret', 'services.idam.clientSecret');
+      this.setLocalSecret('idam-ui-secret', 'services.idam.clientSecret');
       this.setLocalSecret('s2s-secret', 'services.authProvider.secret');
-      this.setLocalSecret('postcode-lookup-token', 'services.postcodeLookup.token');
+     // this.setLocalSecret('postcode-lookup-token', 'services.postcodeLookup.token');
       // this.setLocalSecret('idam-systemupdate-username', 'services.idam.systemUsername');
       // this.setLocalSecret('idam-systemupdate-password', 'services.idam.systemPassword');
       // this.setLocalSecret('e2e-test-user-password', 'e2e.userTestPassword');
-      this.setLocalSecret('adoption-pcq-token', 'services.equalityAndDiversity.tokenKey');
+      //this.setLocalSecret('adoption-pcq-token', 'services.equalityAndDiversity.tokenKey');
     }
   }
 
@@ -39,7 +39,7 @@ export class PropertiesVolume {
    * Load a secret from the AAT vault using azure cli
    */
   private setLocalSecret(secret: string, toPath: string): void {
-    const result = execSync(`az keyvault secret show --vault-name adoption-aat -o tsv --query value --name ${secret}`);
+    const result = execSync(`az keyvault secret show --vault-name ds-aat -o tsv --query value --name ${secret}`);
     set(config, toPath, result.toString().replace('\n', ''));
   }
 }
