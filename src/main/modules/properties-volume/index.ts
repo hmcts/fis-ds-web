c4233a8116c3ad6e8b5ad946f37df242442af47a
Rahul Sanjava <rahul.sanjava@HMCTS.NET>
2022-03-07 15:17:31 +0000

    correct secret paths

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -9,15 +9,15 @@ export class PropertiesVolume {
   enableFor(app: Application): void {
     if (!app.locals.developmentMode) {
       propertiesVolume.addTo(config);
-      this.setSecret('secrets.adoption.AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');
-      this.setSecret('secrets.adoption.idam-secret', 'services.idam.clientSecret');
-      this.setSecret('secrets.adoption.idam-system-user-name', 'services.idam.systemUsername');
-      this.setSecret('secrets.adoption.idam-system-user-password', 'services.idam.systemPassword');
-      this.setSecret('secrets.adoption.redis-access-key', 'session.redis.key');
-      this.setSecret('secrets.adoption.redis-access-key', 'session.secret');
-      this.setSecret('secrets.adoption.s2s-secret', 'services.authProvider.secret');
-      this.setSecret('secrets.adoption.postcode-lookup-token', 'services.postcodeLookup.token');
-      this.setSecret('secrets.adoption.adoption-pcq-token', 'services.equalityAndDiversity.tokenKey');
+      this.setSecret('secrets.ds.AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');
+      this.setSecret('secrets.ds.idam-secret', 'services.idam.clientSecret');
+      this.setSecret('secrets.ds.idam-system-user-name', 'services.idam.systemUsername');
+      this.setSecret('secrets.ds.idam-system-user-password', 'services.idam.systemPassword');
+      this.setSecret('secrets.ds.redis-access-key', 'session.redis.key');
+      this.setSecret('secrets.ds.redis-access-key', 'session.secret');
+      this.setSecret('secrets.ds.s2s-secret', 'services.authProvider.secret');
+      this.setSecret('secrets.ds.postcode-lookup-token', 'services.postcodeLookup.token');
+      this.setSecret('secrets.ds.adoption-pcq-token', 'services.equalityAndDiversity.tokenKey');
     } else {
       this.setLocalSecret('idam-secret', 'services.idam.clientSecret');
       this.setLocalSecret('s2s-secret', 'services.authProvider.secret');
