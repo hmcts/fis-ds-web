c0253c849c01e80262479deb30af12e99e490b47
Amit Vishwakarma <amit.vishwakarma@hmcts.net>
2022-03-09 23:55:46 +0000

    ISBD-68 codecept framework support added

---
+++ src/test/e2e/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  citizenUserOne: {
+    email: process.env.CITIZEN_USERNAME,
+    password: process.env.CITIZEN_PASSWORD,
+  },
+
+  baseUrl: process.env.DS_UI_URL || 'https://ds-ui-staging.service.core-compute-aat.internal/',
+
+  definition: {
+    jurisdiction: 'PUBLICLAW',
+    jurisdictionFullDesc: 'Public Law',
+    caseType: 'CARE_SUPERVISION_EPO',
+    caseTypeFullDesc: 'Care, supervision and EPOs',
+  },
+  // files
+  testFile: './src/test/e2e/fixtures/testFiles/mockFile.txt',
+  testPdfFile: './src/test/e2e/fixtures/testFiles/mockFile.pdf',
+  testWordFile: './src/test/e2e/fixtures/testFiles/mockFile.docx',
+};
