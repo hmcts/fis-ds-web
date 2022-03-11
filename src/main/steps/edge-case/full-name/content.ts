58c1d4356e4e325f4558a8d1cf79e0f25fa7eebe
m_774151 <murshad.haque@HMCTS.NET>
2022-03-11 09:42:45 +0000

    Implementation of full name screen

---
+++ src/main/steps/edge-case/full-name/content.ts
@@ -0,0 +1,54 @@
+import { TranslationFn } from '../../../app/controller/GetController';
+import { FormContent, FormFields } from '../../../app/form/Form';
+import { form as fullNameForm, generateContent as fullNameGenerateContent } from '../../common/components/full-name';
+
+export const en = (): Record<string, unknown> => ({
+  section: 'Applicant',
+  title: "What's your full name?",
+  errors: {
+    applicant1FirstNames: {
+      required: 'Enter your first names',
+    },
+    applicant1LastNames: {
+      required: 'Enter your last names',
+    },
+  },
+});
+
+export const cy = (): Record<string, unknown> => ({
+  section: 'Primary applicant (in Welsh)',
+  title: "What's your full name? (in Welsh)",
+  errors: {
+    applicant1FirstNames: {
+      required: 'Enter your first names (in Welsh)',
+    },
+    applicant1LastNames: {
+      required: 'Enter your last names (in Welsh)',
+    },
+  },
+});
+
+const fullNameFormFields = fullNameForm.fields as FormFields;
+export const form: FormContent = {
+  fields: {
+    applicant1FirstNames: fullNameFormFields.firstNames,
+    applicant1LastNames: fullNameFormFields.lastNames,
+  },
+  submit: {
+    text: l => l.continue,
+  },
+};
+
+const languages = {
+  en,
+  cy,
+};
+
+export const generateContent: TranslationFn = content => {
+  const fullNameContent = fullNameGenerateContent(content);
+  return {
+    ...fullNameContent,
+    ...languages[content.language](),
+    form,
+  };
+};
