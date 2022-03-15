6dae8bede4c1fd64132058300ab1d6d078aeed3e
Shilpa-Krishna <rshilpa200@gmail.com>
2022-03-15 15:47:06 +0000

    smoke test changes

--- src/test/e2e/tests/smoke_test.js
+++ src/test/e2e/tests/smoke_test.js
@@ -3,6 +3,6 @@ const config = require('../config');
 Feature('Smoke tests @smoke-tests').retry(1);
 
 Scenario('Sign in as citizen and create a case', async ({ loginPage, landingPage }) => {
-  await loginPage.createCitizenUserAndSignIn();
+  await loginPage.SignInUser();
   await landingPage.seeTheLandingPage();
 });
