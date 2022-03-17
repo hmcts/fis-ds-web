76961a19a24dbb7cd1748e9b4a9667b357693ffc
praveenrach <praveen.rachapally@HMCTS.NET>
2022-03-17 12:02:23 +0000

    added code for validation fname, lname

--- src/main/steps/edge-case/full-name/content.ts
+++ src/main/steps/edge-case/full-name/content.ts
@@ -1,35 +1,43 @@
 import { TranslationFn } from '../../../app/controller/GetController';
 import { FormContent, FormFields } from '../../../app/form/Form';
+import { CommonContent } from '../../../steps/common/common.content';
 import { form as fullNameForm, generateContent as fullNameGenerateContent } from '../../common/components/full-name';
 
-export const en = (): Record<string, unknown> => ({
-  section: 'Applicant',
-  title: "What's your full name?",
-  errors: {
-    applicant1FirstNames: {
-      required: 'Enter your first names',
+export const en = ({ userCase }: CommonContent): Record<string, unknown> => {
+  const section = 'Applicant';
+  return {
+    section,
+    title: "What's your full name?",
+    errors: {
+      applicant1FirstNames: {
+        required: 'Enter your first names',
+      },
+      applicant1LastNames: {
+        required: 'Enter your last names',
+      },
     },
-    applicant1LastNames: {
-      required: 'Enter your last names',
-    },
-  },
-});
+  };
+};
 
-export const cy = (): Record<string, unknown> => ({
-  section: 'Applicant (in Welsh)',
-  title: "What's your full name? (in Welsh)",
-  errors: {
-    applicant1FirstNames: {
-      required: 'Enter your first names (in Welsh)',
+export const cy = ({ userCase }: CommonContent): Record<string, unknown> => {
+  const section = 'Applicant (in Welsh)';
+  return {
+    section,
+    title: "What's your full name? (in Welsh)",
+    errors: {
+      applicant1FirstNames: {
+        required: 'Enter your first names (in Welsh)',
+      },
+      applicant1LastNames: {
+        required: 'Enter your last names (in Welsh)',
+      },
     },
-    applicant1LastNames: {
-      required: 'Enter your last names (in Welsh)',
-    },
-  },
-});
+  };
+};
 
 const fullNameFormFields = fullNameForm.fields as FormFields;
 export const form: FormContent = {
+ 
   fields: {
     applicant1FirstNames: fullNameFormFields.firstNames,
     applicant1LastNames: fullNameFormFields.lastNames,
@@ -48,7 +56,7 @@ export const generateContent: TranslationFn = content => {
   const fullNameContent = fullNameGenerateContent(content);
   return {
     ...fullNameContent,
-    ...languages[content.language](),
+    ...languages[content.language](content),
     form,
   };
 };
