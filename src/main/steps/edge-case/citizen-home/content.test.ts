988292e5fd24363323d7d6fc962313d932822ac6
Mohammed Murshad Haque <murshad.haque@HMCTS.NET>
2022-03-22 01:51:11 +0000

    ISDB-60: Implementation of citizen home page.

--- src/main/steps/edge-case/citizen-home/content.test.ts
+++ src/main/steps/edge-case/citizen-home/content.test.ts
@@ -1,5 +1,4 @@
 import { FormContent, FormFields, FormOptions } from '../../../app/form/Form';
-//import { isFieldFilledIn } from '../../../app/form/validation';
 import { CommonContent, generatePageContent } from '../../common/common.content';
 
 import { generateContent } from './content';
@@ -27,18 +26,27 @@ describe('citizen-home content', () => {
     const generatedContent = generateContent(commonContent);
     const form = generatedContent.form as FormContent;
     const fields = form.fields as FormFields;
-    const selectJurisdictionField = fields.selectJurisdiction as FormOptions;
-    expect(selectJurisdictionField.type).toBe('radios');
-    expect(selectJurisdictionField.classes).toBe('govuk-radios');
-    expect((selectJurisdictionField.label as Function)(generatedContent)).toBe('Select Jurisdiction');
-    expect((selectJurisdictionField.values[0].label as Function)(generatedContent)).toBe('Family');
-    expect((selectJurisdictionField.values[1].label as Function)(generatedContent)).toBe('Tribunals');
+    const citizenHomeField = fields.citizenHome as FormOptions;
+    expect(citizenHomeField.type).toBe('radios');
+    expect(citizenHomeField.classes).toBe('govuk-radios');
+    expect((citizenHomeField.label as Function)(generatedContent)).toBe('Select Jurisdiction');
+    expect((citizenHomeField.hint as Function)(generatedContent)).toBe(
+      'This is the area of law your application sits under. For example adoption and child custody are under family law, genter recognition and disputes are under tribunals'
+    );
+    expect((citizenHomeField.values[0].label as Function)(generatedContent)).toBe('Family');
+    expect((citizenHomeField.values[1].label as Function)(generatedContent)).toBe('Tribunals');
   });
 
   test('should contain submit button', () => {
     const generatedContent = generateContent(commonContent);
     const form = generatedContent.form as FormContent;
     expect((form.submit.text as Function)(generatePageContent({ language: 'en' }))).toBe('Save and continue');
+  });
+
+  test('should contain cancel button', () => {
+    const generatedContent = generateContent(commonContent);
+    const form = generatedContent.form as FormContent;
+    expect((form.cancel?.text as Function)(generatePageContent({ language: 'en' }))).toBe('Cancel');
   });
 });
 /* eslint-enable @typescript-eslint/ban-types */
--- src/main/steps/edge-case/citizen-home/content.ts
+++ src/main/steps/edge-case/citizen-home/content.ts
@@ -1,44 +1,51 @@
 import { TranslationFn } from '../../../app/controller/GetController';
 import { FormContent } from '../../../app/form/Form';
+import { isFieldFilledIn } from '../../../app/form/validation';
 
 const en = () => ({
   continue: 'Continue',
   label: 'Select Jurisdiction',
   one: 'Family',
   two: 'Tribunals',
+  hint: 'This is the area of law your application sits under. For example adoption and child custody are under family law, genter recognition and disputes are under tribunals',
   errors: {
-    applyingWith: {
-      required: 'Select the jurisdiction type',
+    citizenHome: {
+      required: 'Select Jurisdiction Type',
     },
   },
 });
 
 const cy = () => ({
-  continue: 'Continue',
+  continue: 'Continue (in welsh)',
   label: 'Select Jurisdiction (in welsh)',
   one: 'Family (in welsh)',
   two: 'Tribunals (in welsh)',
   errors: {
-    selectJurisdiction: {
-      required: 'Select the jurisdiction type (in welsh)',
+    citizenHome: {
+      required: 'Select Jurisdiction Type (in welsh)',
     },
   },
 });
 
 export const form: FormContent = {
   fields: {
-    selectJurisdiction: {
+    citizenHome: {
       type: 'radios',
       classes: 'govuk-radios',
       label: l => l.label,
+      hint: h => h.hint,
       values: [
         { label: l => l.one, value: 'family' },
         { label: l => l.two, value: 'tribunals' },
       ],
+      validator: isFieldFilledIn,
     },
   },
   submit: {
     text: l => l.continue,
+  },
+  cancel: {
+    text: c => c.cancel,
   },
 };
 
