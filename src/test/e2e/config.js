fcb7a5a0358f261d9b18dbcda9009732a8add5d5
Amit <vishwa.amit@gmail.com>
2022-03-22 11:33:20 +0000

    smoke test fixes

--- src/test/e2e/config.js
+++ src/test/e2e/config.js
@@ -4,16 +4,5 @@ module.exports = {
     password: process.env.CITIZEN_PASSWORD,
   },
 
-  baseUrl: process.env.DS_UI_URL || 'https://ds-ui.aat.platform.hmcts.net/',
-
-  definition: {
-    jurisdiction: 'PUBLICLAW',
-    jurisdictionFullDesc: 'Public Law',
-    caseType: 'CARE_SUPERVISION_EPO',
-    caseTypeFullDesc: 'Care, supervision and EPOs',
-  },
-  // files
-  testFile: './src/test/e2e/fixtures/testFiles/mockFile.txt',
-  testPdfFile: './src/test/e2e/fixtures/testFiles/mockFile.pdf',
-  testWordFile: './src/test/e2e/fixtures/testFiles/mockFile.docx',
+  baseUrl: process.env.DS_UI_URL || 'https://ds-ui.aat.platform.hmcts.net/'
 };
--- src/test/e2e/pages/login.page.js
+++ src/test/e2e/pages/login.page.js
@@ -10,6 +10,7 @@ module.exports = {
 
   async SignInUser() {
     console.log('User using the URL= ' + config.baseUrl);
+    await I.goToPage(config.baseUrl);
     await I.fillField(this.fields.username, "ds-ui2022@mailinator.com");
     await I.fillField(this.fields.password, "Automation123");
     await I.click(this.fields.submit);
