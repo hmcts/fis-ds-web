e6aa4e15842960a11a44dbb4bbb87efef04e6a01
Amit <vishwa.amit@gmail.com>
2022-03-31 15:11:22 +0100

    secret read from vault and enabling smoketest

--- src/test/e2e/config.js
+++ src/test/e2e/config.js
@@ -4,7 +4,7 @@ module.exports = {
     password: process.env.CITIZEN_PASSWORD,
   },
 
-  baseUrl: process.env.DS_UI_URL || 'https://fis-ds-web.aat.platform.hmcts.net/',
+  baseUrl: process.env.DS_UI_URL || 'https://fis-ds-web-staging.service.core-compute-aat.internal',
 
   definition: {
     jurisdiction: 'PUBLICLAW',
