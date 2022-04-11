2c235f442b962b727f7d52140e4ee904097cd871
Abdul Daim <abdul.daim@hmcts.net>
2022-04-11 11:21:35 +0100

    Added documents upload functionality

--- src/main/routes.ts
+++ src/main/routes.ts
@@ -11,16 +11,23 @@ import { HomeGetController } from './steps/home/get';
 import { SaveSignOutGetController } from './steps/save-sign-out/get';
 // import { TaskListGetController } from './steps/task-list/get';
 import { TimedOutGetController } from './steps/timed-out/get';
-import {UploadDocumentPOSTController} from './steps/edge-case/upload-your-documents/post'
+//import {UploadDocumentPOSTController} from './steps/edge-case/upload-your-documents/post'
+import { DocumentManagerController } from './app/document/DocumentManagementController';
+import multer from 'multer';
 import {
   CSRF_TOKEN_ERROR_URL,
   HOME_URL,
   KEEP_ALIVE_URL,
   SAVE_AND_SIGN_OUT,
+  //DOCUMENT_UPLOAD_URL,
   DOCUMENT_UPLOAD_URL,
   // TASK_LIST_URL,
   TIMED_OUT_URL,
 } from './steps/urls';
+
+
+const handleUploads = multer();
+
 
 export class Routes {
   public enableFor(app: Application): void {
@@ -31,6 +38,17 @@ export class Routes {
     app.get(HOME_URL, errorHandler(new HomeGetController().get));
     app.get(SAVE_AND_SIGN_OUT, errorHandler(new SaveSignOutGetController().get));
     app.get(TIMED_OUT_URL, errorHandler(new TimedOutGetController().get));
+
+
+    /**
+     * @DocumentManager
+     */
+    const documentManagerController = new DocumentManagerController();
+    app.post(DOCUMENT_UPLOAD_URL, handleUploads.array('files[]', 5), errorHandler(documentManagerController.post));
+    app.get(`${DOCUMENT_UPLOAD_URL}/delete/:index`, errorHandler(documentManagerController.delete));
+
+
+
 
     for (const step of stepsWithContent) {
       const files = fs.readdirSync(`${step.stepDir}`);
@@ -47,7 +65,7 @@ export class Routes {
         const postController = postControllerFileName
           ? require(`${step.stepDir}/${postControllerFileName}`).default
           : PostController;
-        app.post(step.url, errorHandler(new postController(step.form.fields).post));
+          app.post(step.url, errorHandler(new postController(step.form.fields).post));
       }
     }
 
