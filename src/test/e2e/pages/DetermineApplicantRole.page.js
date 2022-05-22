2aa08531b9f079a91f341c2871daf652a00ab863
Amit <vishwa.amit@gmail.com>
2022-05-22 09:32:21 +0100

    updated tests to add determination role page

---
+++ src/test/e2e/pages/DetermineApplicantRole.page.js
@@ -0,0 +1,23 @@
+const { I } = inject();
+var assert = require('assert');
+
+module.exports = {
+  fields: {
+    ApplyingForSomeone: 'input[id$="applyingForSelf-2"]',
+    ApplyingForYourself: 'input[id$="applyingForSelf"]'
+  },
+  async DetermineApplicant(applyingForSomeone) {
+    await I.wait(2);
+    await I.see('Are you named as the applicant on the on the application form you are submitting?');
+    if(applyingForSomeone)
+    {   
+        await I.click(this.fields.ApplyingForSomeone);
+    }
+    else{
+        await I.click(this.fields.ApplyingForYourself);
+    }
+    await I.wait(2);
+    await I.click('Continue');
+    await I.wait('4');
+  },
+};
