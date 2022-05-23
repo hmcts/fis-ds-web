17bd901cf211779b351e441f589460f1be36dbf0
Rahul Sanjava <rahul.sanjava@HMCTS.NET>
2022-05-23 15:59:38 +0100

    fix: correct e2e test

--- src/test/e2e/pages/DetermineApplicantRole.page.js
+++ src/test/e2e/pages/DetermineApplicantRole.page.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   async DetermineApplicant(applyingForSomeone) {
     await I.wait(2);
-    await I.see('Are you named as the applicant on the on the application form you are submitting?');
+    await I.see('Are you named as the applicant on the application form you are submitting?');
     if(applyingForSomeone)
     {   
         await I.click(this.fields.ApplyingForSomeone);
