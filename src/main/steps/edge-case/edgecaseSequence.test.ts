39e100ef789b916bbdc8aef9e9da6dcb864900f7
Abdul Daim <abdul.daim@hmcts.net>
2022-05-24 15:42:49 +0100

    fixed lint issues

--- src/main/steps/edge-case/edgecaseSequence.test.ts
+++ src/main/steps/edge-case/edgecaseSequence.test.ts
@@ -1,6 +1,3 @@
-
 describe('Sequence must match respective path', () => {
-  test('should contain 12 entries in applicant 1 screen sequence', () => {
-   
-  });
+  test('should contain 12 entries in applicant 1 screen sequence', () => {});
 });
