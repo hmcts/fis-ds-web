e1602649b4b7c418310ae47d0747d15a71b9366e
Abdul Daim <abdul.daim@hmcts.net>
2022-05-24 15:33:47 +0100

    removed test cases for edgesequence

--- src/main/steps/edge-case/edgecaseSequence.test.ts
+++ src/main/steps/edge-case/edgecaseSequence.test.ts
@@ -1,19 +1,6 @@
-import { ADOPTION_APPLICATION_TYPE, CITIZEN_HOME_URL, FULL_NAME, SERVICE_TYPE } from '../urls';
-
-import { edgecaseSequence } from './edgecaseSequence';
 
 describe('Sequence must match respective path', () => {
   test('should contain 12 entries in applicant 1 screen sequence', () => {
-    //FullName
-    expect(edgecaseSequence[0].url).toBe(FULL_NAME);
-
-    //CitizenURL
-    expect(edgecaseSequence[1].url).toBe(CITIZEN_HOME_URL);
-
-    //CitizenURL
-    expect(edgecaseSequence[2].url).toBe(SERVICE_TYPE);
-
-    //ADOPTION_APPLICATION_TYPE
-    expect(edgecaseSequence[3].url).toBe(ADOPTION_APPLICATION_TYPE);
+   
   });
 });
