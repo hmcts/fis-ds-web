98f6041ab228f86ab062ac900c02c8b972c69101
Abdul Daim <abdul.daim@hmcts.net>
2022-05-24 15:29:44 +0100

    fixed lint and code issues

--- src/main/steps/edge-case/edgecaseSequence.test.ts
+++ src/main/steps/edge-case/edgecaseSequence.test.ts
@@ -1,33 +1,19 @@
-import {edgecaseSequence} from './edgecaseSequence'
-import {
-  ADOPTION_APPLICATION_TYPE,
-  CITIZEN_HOME_URL,
+import { ADOPTION_APPLICATION_TYPE, CITIZEN_HOME_URL, FULL_NAME, SERVICE_TYPE } from '../urls';
 
-  FULL_NAME,
-
-  SERVICE_TYPE,
-
-} from '../urls';
-
-
+import { edgecaseSequence } from './edgecaseSequence';
 
 describe('Sequence must match respective path', () => {
   test('should contain 12 entries in applicant 1 screen sequence', () => {
-  
-    //FullName 
-    expect(edgecaseSequence[0].url).toBe(FULL_NAME)
+    //FullName
+    expect(edgecaseSequence[0].url).toBe(FULL_NAME);
 
     //CitizenURL
-    expect(edgecaseSequence[1].url).toBe(CITIZEN_HOME_URL) 
+    expect(edgecaseSequence[1].url).toBe(CITIZEN_HOME_URL);
 
     //CitizenURL
-    expect(edgecaseSequence[2].url).toBe(SERVICE_TYPE) 
+    expect(edgecaseSequence[2].url).toBe(SERVICE_TYPE);
 
     //ADOPTION_APPLICATION_TYPE
-    expect(edgecaseSequence[3].url).toBe(ADOPTION_APPLICATION_TYPE) 
-
-
-    expect(edgecaseSequence.map(item => item.hasOwnProperty('getNextStep')).every(i => true)).toBe(true);
-    expect(edgecaseSequence.map(item => item.hasOwnProperty('url')).every(i => true)).toBe(true);
+    expect(edgecaseSequence[3].url).toBe(ADOPTION_APPLICATION_TYPE);
   });
 });
