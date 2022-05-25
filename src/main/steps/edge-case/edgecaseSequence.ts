554f1fd1a56178dcdde14d4d670fe53e47259e18
Abdul Daim <abdul.daim@hmcts.net>
2022-05-25 14:13:07 +0100

    updated sequences.

--- src/main/steps/edge-case/edgecaseSequence.ts
+++ src/main/steps/edge-case/edgecaseSequence.ts
@@ -1,18 +1,13 @@
-import { YesOrNo } from '../../app/case/definition';
 import { Sections, Step } from '../constants';
 import {
   ADDITIONAL_DOCUMENTS_UPLOAD,
-  ADOPTION_APPLICATION_TYPE,
-  CITIZEN_HOME_URL,
   CONTACT_DETAILS,
   DATE_OF_BIRTH,
   FIND_ADDRESS,
   FULL_NAME,
   MANUAL_ADDRESS,
   PAY_YOUR_FEE,
-  PRIVATE_LAW_APPLICATION_TYPE,
   SELECT_ADDRESS,
-  SERVICE_TYPE,
   UPLOAD_YOUR_DOCUMENTS,
   USER_ROLE,
 } from '../urls';
@@ -28,26 +23,7 @@ export const edgecaseSequence: Step[] = [
     showInSection: Sections.AboutEdgeCase,
     getNextStep: () => DATE_OF_BIRTH,
   },
-  {
-    url: CITIZEN_HOME_URL,
-    showInSection: Sections.AboutEdgeCase,
-    getNextStep: () => SERVICE_TYPE,
-  },
-  {
-    url: SERVICE_TYPE,
-    showInSection: Sections.AboutEdgeCase,
-    getNextStep: data => (data.serviceType === YesOrNo.YES ? ADOPTION_APPLICATION_TYPE : PRIVATE_LAW_APPLICATION_TYPE),
-  },
-  {
-    url: ADOPTION_APPLICATION_TYPE,
-    showInSection: Sections.AboutEdgeCase,
-    getNextStep: () => FULL_NAME,
-  },
-  {
-    url: PRIVATE_LAW_APPLICATION_TYPE,
-    showInSection: Sections.AboutEdgeCase,
-    getNextStep: () => FULL_NAME,
-  },
+
   {
     url: DATE_OF_BIRTH,
     showInSection: Sections.AboutEdgeCase,
@@ -85,6 +61,6 @@ export const edgecaseSequence: Step[] = [
   },
   {
     url: PAY_YOUR_FEE,
-    getNextStep: () => CITIZEN_HOME_URL,
+    getNextStep: () => USER_ROLE,
   },
 ];
