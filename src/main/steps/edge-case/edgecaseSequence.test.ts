06c428502b3fc181d0c59c17953b4997370ccce6
Abdul Daim <abdul.daim@hmcts.net>
2022-05-25 15:03:45 +0100

    fixed linting issues

--- src/main/steps/edge-case/edgecaseSequence.test.ts
+++ src/main/steps/edge-case/edgecaseSequence.test.ts
@@ -1,4 +1,3 @@
-
 import {
   ADDITIONAL_DOCUMENTS_UPLOAD,
   CONTACT_DETAILS,
@@ -11,13 +10,12 @@ import {
   UPLOAD_YOUR_DOCUMENTS,
   USER_ROLE,
 } from '../urls';
-import {edgecaseSequence} from './edgecaseSequence'
 
+import { edgecaseSequence } from './edgecaseSequence';
 
 describe('Sequence must match respective path', () => {
   test('must match the path', () => {
     expect(edgecaseSequence).toHaveLength(10);
-
 
     expect(edgecaseSequence[0].url).toBe(USER_ROLE);
     expect(edgecaseSequence[0].getNextStep({})).toBe(FULL_NAME);
@@ -28,44 +26,27 @@ describe('Sequence must match respective path', () => {
     expect(edgecaseSequence[2].url).toBe(DATE_OF_BIRTH);
     expect(edgecaseSequence[2].getNextStep({})).toBe(FIND_ADDRESS);
 
-
     expect(edgecaseSequence[3].url).toBe(FIND_ADDRESS);
     expect(edgecaseSequence[3].getNextStep({})).toBe(SELECT_ADDRESS);
-
-
-   
-
 
     expect(edgecaseSequence[5].url).toBe(MANUAL_ADDRESS);
     expect(edgecaseSequence[5].getNextStep({})).toBe(CONTACT_DETAILS);
 
-
     expect(edgecaseSequence[6].url).toBe(CONTACT_DETAILS);
     expect(edgecaseSequence[6].getNextStep({})).toBe(UPLOAD_YOUR_DOCUMENTS);
-
 
     expect(edgecaseSequence[7].url).toBe(UPLOAD_YOUR_DOCUMENTS);
     expect(edgecaseSequence[7].getNextStep({})).toBe(ADDITIONAL_DOCUMENTS_UPLOAD);
 
-
     expect(edgecaseSequence[8].url).toBe(ADDITIONAL_DOCUMENTS_UPLOAD);
     expect(edgecaseSequence[8].getNextStep({})).toBe(PAY_YOUR_FEE);
 
-
     expect(edgecaseSequence[9].url).toBe(PAY_YOUR_FEE);
     expect(edgecaseSequence[9].getNextStep({})).toBe(USER_ROLE);
-
-
-
-
-    
-
   });
 });
 
-
-
-describe('Sequence must match respective path', () => {
+describe('sample test', () => {
   test('should contain 12 entries in applicant 1 screen sequence', () => {
     expect(1).toBe(1);
   });
