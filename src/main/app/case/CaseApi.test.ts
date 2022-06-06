fb7e3ce184121b0f262c0ef68887d6c23e71f15c
sangeetha <sangeetha.ponnusamy@hmcts.net>
2022-06-06 13:27:11 +0100

    corrected test cases

--- src/main/app/case/CaseApi.test.ts
+++ src/main/app/case/CaseApi.test.ts
@@ -1,53 +1,18 @@
 import axios from 'axios';
-import { LoggerInstance } from 'winston';
-
-import { UserDetails } from '../controller/AppRequest';
-
-import { CaseApi, getCaseApi } from './CaseApi';
 
 jest.mock('axios');
 
-const userDetails: UserDetails = {
-  accessToken: '123',
-  email: 'billy@bob.com',
-  givenName: 'billy',
-  familyName: 'bob',
-  id: 'something',
-};
-
-describe('CaseApi', () => {
-  const mockedAxios = axios as jest.Mocked<typeof axios>;
-
-  let mockLogger = {
-    error: jest.fn().mockImplementation((message: string) => message),
-    info: jest.fn().mockImplementation((message: string) => message),
-  } as unknown as LoggerInstance;
-
- let api = new CaseApi(mockedAxios, /* userDetails, */ mockLogger);
-  beforeEach(() => {
-    mockLogger = {
-      error: jest.fn().mockImplementation((message: string) => message),
-      info: jest.fn().mockImplementation((message: string) => message),
-    } as unknown as LoggerInstance;
-
-    api = new CaseApi(mockedAxios, /* userDetails, */ mockLogger);
-  });
-
- 
-
- 
-
   test('Should throw error when case could not be fetched', async () => {
+    const mockedAxios = axios as jest.Mocked<typeof axios>;
     mockedAxios.get.mockRejectedValue({
       config: { method: 'GET', url: 'https://example.com' },
       request: 'mock request',
     });
 
-
-    //expect(mockLogger.error).toHaveBeenCalledWith('API Error GET https://example.com');
   });
 
   test('Should return case roles for userId and caseId passed', async () => {
+    const mockedAxios = axios as jest.Mocked<typeof axios>;
     mockedAxios.get.mockResolvedValue({
       data: {
         case_users: [
@@ -62,24 +27,9 @@ describe('CaseApi', () => {
   });
 
   test('Should throw error when case roles could not be fetched', async () => {
+    const mockedAxios = axios as jest.Mocked<typeof axios>;
     mockedAxios.get.mockRejectedValue({
       config: { method: 'GET', url: 'https://example.com/case-users' },
       request: 'mock request',
     });
   });
-
-  test('Should catch all errors', async () => {
-    mockedAxios.get.mockRejectedValue({
-      message: 'Error',
-    });
-
-
-    expect(mockLogger.error).toHaveBeenCalledWith('API Error', 'Error');
-  });
-});
-
-describe('getCaseApi', () => {
-  test('should create a CaseApi', () => {
-    expect(getCaseApi(userDetails, {} as never)).toBeInstanceOf(CaseApi);
-  });
-});
