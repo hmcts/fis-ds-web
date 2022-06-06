8ee7e8d46c9190359f092e7275d0d0440c753068
sangeetha <sangeetha.ponnusamy@hmcts.net>
2022-06-06 11:51:02 +0100

    Updated test cases

--- src/main/app/case/to-api-format.test.ts
+++ src/main/app/case/to-api-format.test.ts
@@ -3,8 +3,7 @@ jest.mock('uuid', () => ({
   v4: v4Mock,
 }));
 
-import { Case, Checkbox } from './case';
-import { YesOrNo } from './definition';
+import { Case } from './case';
 import { toApiFormat } from './to-api-format';
 
 describe('to-api-format', () => {
@@ -15,102 +14,7 @@ describe('to-api-format', () => {
     } as Partial<Case>);
 
     expect(apiFormat).toMatchObject({
-      applicant1HWFReferenceNumber: '',
+      applicant1HWFReferenceNumber: '123-ABC',
     });
-  });
-
-  test.each([
-    {
-      applicant1HelpPayingNeeded: YesOrNo.YES,
-      expected: {
-        applicant1HWFNeedHelp: YesOrNo.YES,
-      },
-    },
-    {
-      applicant1HelpPayingNeeded: YesOrNo.NO,
-      expected: {
-        applicant1HWFNeedHelp: YesOrNo.NO,
-        applicant1HWFAppliedForFees: null,
-        applicant1HWFReferenceNumber: null,
-      },
-    },
-    {
-      applicant1IBelieveApplicationIsTrue: Checkbox.Checked,
-      expected: {
-        applicant1StatementOfTruth: 'Yes',
-      },
-    },
-    {
-      applicant1HasOtherNames: YesOrNo.NO,
-      applicant1AdditionalNames: [],
-      expected: {
-        applicant1AdditionalNames: [],
-      },
-    },
-    {
-      applicant1HasOtherNames: YesOrNo.YES,
-      applicant1AdditionalNames: undefined,
-      expected: {
-        applicant1AdditionalNames: [],
-      },
-    },
-    {
-      applicant2HasOtherNames: YesOrNo.NO,
-      applicant2AdditionalNames: [],
-      expected: {
-        applicant2AdditionalNames: [],
-      },
-    },
-    {
-      applicant2HasOtherNames: YesOrNo.YES,
-      applicant2AdditionalNames: undefined,
-      expected: {
-        applicant2AdditionalNames: [],
-      },
-    },
-    {
-      applicant1AdditionalNationalities: undefined,
-      expected: {
-        applicant1AdditionalNationalities: [],
-      },
-    },
-    {
-      applicant2AdditionalNationalities: undefined,
-      expected: {
-        applicant2AdditionalNationalities: [],
-      },
-    },
-    {
-      childrenAdditionalNationalities: undefined,
-      expected: {
-        childrenAdditionalNationalities: [],
-      },
-    },
-    {
-      placementOrders: undefined,
-      expected: {
-        placementOrders: [],
-      },
-    },
-    {
-      siblings: undefined,
-      expected: {
-        siblings: [],
-      },
-    },
-    {
-      siblings: [{ siblingPlacementOrders: undefined }],
-      expected: {
-        siblings: [{ id: 'MOCK_V4_UUID', value: { siblingPlacementOrders: [] } }],
-      },
-    },
-    {
-      adopAgencyOrLAs: undefined,
-      expected: {
-        adopAgencyOrLAs: [],
-      },
-    },
-  ])('set unreachable answers to null if condition met', ({ expected, ...formData }) => {
-    expect(toApiFormat(formData as Partial<Case>)).toMatchObject(expected);
   });
 });
