aebfa0ab6ca158001f17a80ce3e554467a4b4c27
sangeetha <sangeetha.ponnusamy@hmcts.net>
2022-06-07 10:07:36 +0100

    Added few changes for refactoring

--- src/main/app/controller/GetController.test.ts
+++ src/main/app/controller/GetController.test.ts
@@ -181,19 +181,6 @@ describe('GetController', () => {
     expect(1).toEqual(1);
   });
 
-  describe('parseAndSetReturnUrl', () => {
-    test.each([
-      { returnUrl: undefined, expected: undefined },
-      { returnUrl: '/unknown-url', expected: undefined },
-      { returnUrl: '/applicant1/full-name', expected: '/applicant1/full-name' },
-    ])('correctly parses and sets the return url in session', ({ returnUrl, expected }) => {
-      const controller = new GetController('page', () => ({}));
-      const req = mockRequest({ query: { returnUrl } });
-      controller.parseAndSetReturnUrl(req);
-      expect(req.session.returnUrl).toBe(expected);
-    });
-  });
-
   describe('generatePageContent()', () => {
     test('calls generatePageContent with correct arguments for new sessions', async () => {
       const getContentMock = jest.fn().mockReturnValue({});
