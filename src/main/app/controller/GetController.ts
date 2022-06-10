07509fee524f8d3b5a90ac4981fbeec2c5f72fca
Abdul Daim <abdul.daim@hmcts.net>
2022-06-10 15:12:37 +0100

    made some amendments

--- src/main/app/controller/GetController.ts
+++ src/main/app/controller/GetController.ts
@@ -142,7 +142,8 @@ export class GetController {
           case 'applicationform': {
             try {
               const baseURL = `/doc/dss-orhestration/${docId}/delete`;
-              await DOCUMENT_DELETEMANAGER.delete(baseURL);
+             const response = await DOCUMENT_DELETEMANAGER.delete(baseURL);
+             if(response.statusText === "OK"){
               const sessionObjectOfApplicationDocuments = req.session['caseDocuments'].filter(document => {
                 const { documentId } = document;
                 return documentId !== docId;
@@ -152,6 +153,8 @@ export class GetController {
               this.saveSessionAndRedirect(req, res, () => {
                 res.redirect(UPLOAD_YOUR_DOCUMENTS);
               });
+             }
+             
             } catch (error) {
               console.log(error);
             }
@@ -162,16 +165,19 @@ export class GetController {
           case 'additional': {
             try {
               const baseURL = `/doc/dss-orhestration/${docId}/delete`;
-              await DOCUMENT_DELETEMANAGER.delete(baseURL);
-              const sessionObjectOfAdditionalDocuments = req.session['AddtionalCaseDocuments'].filter(document => {
-                const { documentId } = document;
-                return documentId !== docId;
-              });
-              req.session['AddtionalCaseDocuments'] = sessionObjectOfAdditionalDocuments;
-              console.log({ AddtionalDocuments: req.session['AddtionalCaseDocuments'] });
-              this.saveSessionAndRedirect(req, res, () => {
-                res.redirect(ADDITIONAL_DOCUMENTS_UPLOAD);
-              });
+             const response = await DOCUMENT_DELETEMANAGER.delete(baseURL);
+              if(response.statusText === 'OK'){
+                const sessionObjectOfAdditionalDocuments = req.session['AddtionalCaseDocuments'].filter(document => {
+                  const { documentId } = document;
+                  return documentId !== docId;
+                });
+                req.session['AddtionalCaseDocuments'] = sessionObjectOfAdditionalDocuments;
+                console.log({ AddtionalDocuments: req.session['AddtionalCaseDocuments'] });
+                this.saveSessionAndRedirect(req, res, () => {
+                  res.redirect(ADDITIONAL_DOCUMENTS_UPLOAD);
+                });
+              }
+             
             } catch (error) {
               console.log(error);
             }
--- src/main/modules/timeouts/index.ts
+++ src/main/modules/timeouts/index.ts
@@ -1,33 +1,38 @@
 import config from 'config';
 import { Application } from 'express';
 import { StatusCodes } from 'http-status-codes';
-
+import {DOCUMENT_UPLOAD_URL, ADDITIONAL_DOCUMENTS_UPLOAD} from '../../steps/urls'
 import { AppRequest } from '../../app/controller/AppRequest';
 import { ErrorController, HTTPError } from '../../steps/error/error.controller';
 
 export class LoadTimeouts {
   public enableFor(app: Application): void {
+  
     const timeoutMs = config.get<number>('timeout');
 
     app.use((req, res, next) => {
-      const errorController = new ErrorController();
+      const checkForPaths = req.path.startsWith(DOCUMENT_UPLOAD_URL) || req.path.startsWith(ADDITIONAL_DOCUMENTS_UPLOAD);
 
-      // Set the timeout for all HTTP requests
-      req.setTimeout(timeoutMs, () => {
-        const err = new HTTPError('Request Timeout', StatusCodes.REQUEST_TIMEOUT);
-        errorController.internalServerError(err, req as AppRequest, res);
-      });
-
-      // Set the server response timeout for all HTTP requests
-
-      /**
-       *
-       */
-      res.setTimeout(timeoutMs, () => {
-        const err = new HTTPError('Service Unavailable', StatusCodes.SERVICE_UNAVAILABLE);
-        errorController.internalServerError(err, req as AppRequest, res);
-      });
-
+      if(!checkForPaths){
+        const errorController = new ErrorController();
+        
+        req.setTimeout(timeoutMs, () => {
+          const err = new HTTPError('Request Timeout', StatusCodes.REQUEST_TIMEOUT);
+          errorController.internalServerError(err, req as AppRequest, res);
+        });
+  
+        // Set the server response timeout for all HTTP requests
+  
+        /**
+         *
+         */
+        res.setTimeout(timeoutMs, () => {
+          const err = new HTTPError('Service Unavailable', StatusCodes.SERVICE_UNAVAILABLE);
+          errorController.internalServerError(err, req as AppRequest, res);
+        });
+  
+      }
+    
       next();
     });
   }
