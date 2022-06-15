821e96bd24c3f05cd396efff4664814834650dbc
Abdul Daim <abdul.daim@hmcts.net>
2022-06-15 21:08:54 +0100

    Added redirect if user is logged in

--- src/main/routes/authless/controllers/landing.ts
+++ src/main/routes/authless/controllers/landing.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 import { ResourceReader } from '../../../modules/resourcereader/ResourceReader';
-import { SIGN_IN_URL } from '../../../steps/urls';
+import { SIGN_IN_URL, USER_ROLE } from '../../../steps/urls';
 
 /*** query params for @edgeCaseType */
 const edgeCaseTypeQueryValidations = (req: Request) => {
@@ -13,38 +13,42 @@ const edgeCaseTypeQueryValidations = (req: Request) => {
 
 export const LandingController = (req: Request, res: Response): void => {
   const loginURL = SIGN_IN_URL;
-  try {
-    const resourceLoader = new ResourceReader();
-    resourceLoader.Loader('landing');
-    const Translations = resourceLoader.getFileContents().translations;
+  if (req.session.hasOwnProperty('user')) {
+    res.redirect(USER_ROLE);
+  } else {
+    try {
+      const resourceLoader = new ResourceReader();
+      resourceLoader.Loader('landing');
+      const Translations = resourceLoader.getFileContents().translations;
 
-    /**
-     * @SystemLanguage
-     */
-    const en = Translations.en;
-    const cy = Translations.cy;
+      /**
+       * @SystemLanguage
+       */
+      const en = Translations.en;
+      const cy = Translations.cy;
 
-    let SystemContent = en;
-    let ToggleLanguage = 'cy';
+      let SystemContent = en;
+      let ToggleLanguage = 'cy';
 
-    if (req.query.hasOwnProperty('lang')) {
-      if (req.query['lang'] === 'en') {
-        SystemContent = en;
-        ToggleLanguage = 'cy';
-      } else if (req.query['lang'] === 'cy') {
-        SystemContent = cy;
-        ToggleLanguage = 'en';
-      } else {
-        console.log({ msg: 'Invalid language - Setting default Langauge to English' });
-        SystemContent = en;
-        ToggleLanguage = 'cy';
+      if (req.query.hasOwnProperty('lang')) {
+        if (req.query['lang'] === 'en') {
+          SystemContent = en;
+          ToggleLanguage = 'cy';
+        } else if (req.query['lang'] === 'cy') {
+          SystemContent = cy;
+          ToggleLanguage = 'en';
+        } else {
+          console.log({ msg: 'Invalid language - Setting default Langauge to English' });
+          SystemContent = en;
+          ToggleLanguage = 'cy';
+        }
       }
-    }
 
-    edgeCaseTypeQueryValidations(req);
-    res.render('landing.njk', { loginURL, content: SystemContent, ToggleLanguage });
-  } catch (exception) {
-    console.log({ msg: 'Exception occured while reading the file' + exception });
-    res.render('error');
+      edgeCaseTypeQueryValidations(req);
+      res.render('landing.njk', { loginURL, content: SystemContent, ToggleLanguage });
+    } catch (exception) {
+      console.log({ msg: 'Exception occured while reading the file' + exception });
+      res.render('error');
+    }
   }
 };
