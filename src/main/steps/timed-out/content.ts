e43f77c7b45a38c609263d9852c5e58b896445c4
Abdul Daim <abdul.daim@hmcts.net>
2022-07-01 14:37:02 +0100

    Added content for timeout

--- src/main/steps/timed-out/content.ts
+++ src/main/steps/timed-out/content.ts
@@ -1,17 +1,16 @@
 import { TranslationFn } from '../../app/controller/GetController';
+import { ResourceReader } from '../../modules/resourcereader/ResourceReader';
+
+const resourceLoader = new ResourceReader();
+resourceLoader.Loader('timeout');
+const Translations = resourceLoader.getFileContents().translations;
 
 const en = {
-  title: 'You were signed out to protect your privacy',
-  line1: 'Your application was inactive for more than 20 minutes so you were signed out.',
-  line2: 'Your progress was saved.',
-  signBackInAndContinue: 'Sign back in and continue',
+  ...Translations.en,
 };
 
 const cy: typeof en = {
-  title: 'Cawsoch eich allgofnodi i amddiffyn eich preifatrwydd',
-  line1: "Ni chafodd unrhyw beth ei wneud i'ch cais am dros 20 munud, felly cawsoch eich allgofnodi.",
-  line2: "Mae'r hyn a wnaethoch wedi cael ei gadw.",
-  signBackInAndContinue: 'Mewngofnodi eto a pharhau',
+  ...Translations.cy,
 };
 
 const languages = {
