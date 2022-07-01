3b1812c7ed41837eafc1af312e5b109ff7452bf2
Abdul Daim <abdul.daim@hmcts.net>
2022-07-01 14:49:30 +0100

    cleaned urls and go-back url

--- src/main/assets/js/go-back.ts
+++ src/main/assets/js/go-back.ts
@@ -1,22 +1,11 @@
-import {
-  CHECK_JURISDICTION,
-  JURISDICTION_CONNECTION_SUMMARY,
-  JURISDICTION_INTERSTITIAL_URL,
-  JURISDICTION_MAY_NOT_BE_ABLE_TO,
-  WHERE_YOUR_LIVES_ARE_BASED_URL,
-} from '../../steps/urls';
+import { CHECK_JURISDICTION } from '../../steps/urls';
 
 const backLink: HTMLAnchorElement | null = document.querySelector('.govuk-back-link');
 if (backLink) {
   backLink.onclick = function (e) {
     e.preventDefault();
-    if (document.location.pathname === JURISDICTION_INTERSTITIAL_URL) {
+    if (document.location.pathname === CHECK_JURISDICTION) {
       document.location.pathname = CHECK_JURISDICTION;
-    } else if (
-      document.location.pathname === JURISDICTION_MAY_NOT_BE_ABLE_TO ||
-      document.location.pathname === JURISDICTION_CONNECTION_SUMMARY
-    ) {
-      document.location.pathname = WHERE_YOUR_LIVES_ARE_BASED_URL;
     } else {
       history.go(-1);
     }
