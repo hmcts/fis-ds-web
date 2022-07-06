4902c34b172491c2c72fb2aeed788e4017f14ca8
Abdul Daim <abdul.daim@hmcts.net>
2022-07-06 11:03:03 +0100

    removed savedandSignout and saveneoreSessionTimeout

--- src/main/app/controller/PostController.ts
+++ src/main/app/controller/PostController.ts
@@ -36,7 +36,7 @@ export class PostController<T extends AnyObject> {
     const fields = typeof this.fields === 'function' ? this.fields(req.session.userCase) : this.fields;
     const form = new Form(fields);
 
-    const { saveAndSignOut, saveBeforeSessionTimeout, _csrf, ...formData } = form.getParsedBody(req.body);
+    const { _csrf, ...formData } = form.getParsedBody(req.body);
     await this.saveAndContinue(req, res, form, formData);
   }
 
