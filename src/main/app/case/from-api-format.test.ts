f663b5b00898c1e8c193690f901618f285fb5083
Abdul Daim <abdul.daim@hmcts.net>
2022-07-07 11:57:00 +0100

    added test case for demonstation purposes

--- src/main/app/case/from-api-format.test.ts
+++ src/main/app/case/from-api-format.test.ts
@@ -22,4 +22,22 @@ describe('from-api-format', () => {
       expect(nfdivFormat).toMatchObject({});
     });
   });
+  describe('converting your address between UK and international with locale', () => {
+    test('converts to an international format', () => {
+      const nfdivFormat = fromApiFormat({
+        ...results,
+        applicantHomeAddress: {
+          AddressLine1: 'Line 1',
+          AddressLine2: 'Line 2',
+          AddressLine3: 'Line 3',
+          PostTown: 'Town',
+          County: 'State',
+          PostCode: 'Zip code',
+          Country: 'Country',
+        },
+      } as unknown as CaseData);
+
+      expect(nfdivFormat).toMatchObject({});
+    });
+  });
 });
