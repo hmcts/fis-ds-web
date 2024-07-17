d80c36a10f78fb9138924aeac2a074c8a1c7db75
58060 <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>
2024-07-17 00:23:17 +0000

    Updating Terraform Formatting

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -14,12 +14,12 @@ variable "location" {
   default = "UK South"
 }
 
-variable "env" { }
+variable "env" {}
 
 variable "subscription" {}
 
 variable "jenkins_AAD_objectId" {
-  description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
 variable "tenant_id" {
