5ea3ca0b635e04260da4d864d625efa5641f774d
sealDebolina <116593651+sealDebolina@users.noreply.github.com>
2025-02-11 09:31:10 +0000

    Update to-api-format.ts

--- src/main/app/case/to-api-format.ts
+++ src/main/app/case/to-api-format.ts
@@ -1,4 +1,3 @@
-import dayjs from 'dayjs';
 
 import { Case, CaseDate, formFieldsToCaseMapping, formatCase } from './case';
 import { CaseData } from './definition';
@@ -20,12 +19,3 @@ export const toApiDate = (date: CaseDate | undefined): string => {
   return date.year + '-' + date.month.padStart(2, '0') + '-' + date.day.padStart(2, '0');
 };
 
-export const toDate = (date: string): CaseDate => {
-  return date
-    ? {
-        day: dayjs(date).format('DD'),
-        month: dayjs(date).format('MM'),
-        year: dayjs(date).format('YYYY'),
-      }
-    : { day: '', month: '', year: '' };
-};
