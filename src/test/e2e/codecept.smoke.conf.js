da590cc0602bd5a3343d2309e6d6ad0b616bcc18
Channabasappagouda Patil <109162337+channa-patil@users.noreply.github.com>
2025-03-03 19:03:24 +0000

    Update README.md to trigger master build pipeline (#367)
    * Update README.md to trigger master build pipeline
    
    * fix build issus - updated yarn-audit-known-issues
    
    * Fix docker build issue
    
    * Update yarn audit suppressions
    
    * Temporary smoke test fix
    
    ---------
    
    Co-authored-by: hcarslaw <Hugh.Carslaw@HMCTS.NET>

--- src/test/e2e/codecept.smoke.conf.js
+++ src/test/e2e/codecept.smoke.conf.js
@@ -1,32 +1,29 @@
 exports.config = {
-    tests: './tests/*.js',
-    output: './output',
-    helpers: {
-      Playwright: {
-        // headless mode
-        show: process.env.SHOW_BROWSER_WINDOW || false,
-        // show: true,
-        url: 'http://localhost:3000',
-        waitForTimeout: 60000,
-        getPageTimeout: 60000,
-        waitForAction: 1000,
-        waitForNavigation: 'domcontentloaded',
+  tests: '../tests/*_test.js',
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      show: process.env.SHOW_BROWSER_WINDOW || false,
+      url: 'http://localhost:3000',
+      waitForTimeout: 60000,
+      getPageTimeout: 60000,
+      waitForAction: 1000,
+      waitForNavigation: 'domcontentloaded',
         chrome: {
-          ignoreHTTPSErrors: true,
-          args: [ '--disable-gpu', '--no-sandbox', '--allow-running-insecure-content', '--ignore-certificate-errors']
-        },
-        windowSize: '1280x960'
+        ignoreHTTPSErrors: true,
+        args: [ '--disable-gpu', '--no-sandbox', '--allow-running-insecure-content', '--ignore-certificate-errors']
       },
-      PlaywrightHelpers: { require: './helpers/playwrightHelper.js' }
-    },
-    plugins: {
-      retryFailedStep: {
-        enabled: true,
-        retries: 2,
-        minTimeout: 2000
-      },
-      autoDelay: { enabled: true }
+       windowSize: '1280x960'
     }
-  };
+  },
+  plugins: {
+    retryFailedStep: {
+      enabled: true,
+      retries: 2,
+      minTimeout: 2000
+    },
+    autoDelay: { enabled: true }
+  }
+};
   
   
