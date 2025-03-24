2f113adcbb7e9d04fb3bc9ce749a7b043c4e5123
sealDebolina <debolina.seal@hmcts.net>
2025-03-24 18:06:51 +0000

    test case fix

--- src/main/modules/pcq/controller.test.ts
+++ src/main/modules/pcq/controller.test.ts
@@ -1,8 +1,6 @@
-//import { defaultViewArgs } from '../../../test/unit/utils/defaultViewArgs';
 import { mockRequest } from '../../../test/unit/utils/mockRequest';
 import { mockResponse } from '../../../test/unit/utils/mockResponse';
-//import { generatePageContent } from '../../steps/common/common.content';
-// import { State } from '../case/definition';
+
 
 import { PcqController } from './controller';
 
