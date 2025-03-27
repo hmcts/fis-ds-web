916597cd2165aee8c29dd5db26b0cc5a0460dec8
Caroline-Amanquah-HMCTS <Caroline.Amanquah@HMCTS.NET>
2025-03-27 15:59:10 +0000

    updated paymentHelper test file

--- src/main/steps/edge-case/payments/paymentHelper.test.ts
+++ src/main/steps/edge-case/payments/paymentHelper.test.ts
@@ -1,26 +1,46 @@
 import { mockRequest } from '../../../../test/unit/utils/mockRequest';
+import { getServiceAuthToken } from '../../../app/auth/service/get-service-auth-token';
 
 import { PaymentHelper } from './paymentHelper';
 
-const mockToken = 'authToken';
+jest.mock('../../../app/auth/service/get-service-auth-token');
 
-const dummyCaseID = '2122323';
-
-const InstanceOfPaymentHelper = new PaymentHelper();
 describe('PaymentHelper', () => {
-  const req = mockRequest({});
+  const mockToken = 'authToken';
+  const errorMessage = 'Service Auth Token Fetch Failed';
+  const dummyCaseID = '2122323';
+  const req: any = mockRequest({});
+  req.locals = { logger: { error: jest.fn() } };
+  req.session = {
+    user: { accessToken: mockToken },
+    userCase: { id: dummyCaseID },
+  };
+  req.headers = { host: 'localhost:3001' };
 
-  req.session.user.accessToken = mockToken;
-  req.session.userCase.id = dummyCaseID;
-  req.headers.host = 'localhost:3001';
+  const InstanceOfPaymentHelper = new PaymentHelper();
+
+  beforeEach(() => {
+    (getServiceAuthToken as jest.Mock).mockReset();
+    (getServiceAuthToken as jest.Mock).mockImplementation(() => {
+      throw new Error(errorMessage);
+    });
+  });
+
   test('Should match the output values corresponding to given input', async () => {
+    (getServiceAuthToken as jest.Mock).mockResolvedValue('serviceToken');
     const generateMockSystemCredential = await InstanceOfPaymentHelper.SystemCredentailsToApiData(req);
 
     expect(generateMockSystemCredential.Authorization).toEqual(mockToken);
     expect(generateMockSystemCredential.id).toEqual(dummyCaseID);
   });
 
+  test('Service token fetch failure should trigger error logging', async () => {
+    await InstanceOfPaymentHelper.SystemCredentailsToApiData(req);
+    expect(req.locals.logger.error).toHaveBeenCalledWith(expect.any(Error));
+  });
+
   test('Shouldn"t match the output values corresponding to given input', async () => {
+    (getServiceAuthToken as jest.Mock).mockResolvedValue('serviceToken');
     const generateMockSystemCredential = await InstanceOfPaymentHelper.SystemCredentailsToApiData(req);
 
     expect(generateMockSystemCredential).not.toEqual({
